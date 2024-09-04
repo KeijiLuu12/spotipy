43b106343fd1092e33dd7fb56c14239af17eff82
KeijiLuu12 <117475984+KeijiLuu12@users.noreply.github.com>
2024-09-04 13:49:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,17 +39,22 @@ def top_artists():
 
     return render_template('top-artist.html', artists=top_artist ) # page that displays when user first clicks login and successfully links their account
 
+@app.route('/generated')
+def generated():
+
+    return render_template('playlist-generated.html')
+
 @app.route('/top-artist-medium') #following functions utilize Spoify API to get information on top artist/songs for different time frames and stores that information as a json file to be accessed when the user clicks on different timeframe options within the top-artist page so JS can dynmanically update information on the page
 def top_artist_timeframe_medium():
 
-    top_artist = recommendation.get_top_artists('medium_term', 5) #top artists of past 6 months
+    top_artist = recommendation.get_top_artists('medium_term', 50) #top artists of past 6 months
 
     artist_json = [] # creates list representing json file
-    index = 1 #prob can taken out oof 
-    for artist in top_artist: #loops through json file for each artist to get imporant data needed from Spotify API like their image and name
+  
+    for artist in top_artist: #loops through json file for each artist to get important data needed from Spotify API like their image and name
 
-        artist_json.append({"id": index, "name": artist[1], "image": artist[2]})
-        index += 1
+        artist_json.append({"name": artist[1], "image": artist[2]})
+       
 
     top_artist = jsonify(artist_json)
 
@@ -58,14 +63,14 @@ def top_artist_timeframe_medium():
 @app.route('/top-artist-short') 
 def top_artist_timeframe_short():
 
-    top_artist = recommendation.get_top_artists('short_term', 5)
+    top_artist = recommendation.get_top_artists('short_term', 50)
 
     artist_json = []
-    index = 1
+   
     for artist in top_artist:
 
-        artist_json.append({"id": index, "name": artist[1], "image": artist[2]})
-        index += 1
+        artist_json.append({"name": artist[1], "image": artist[2]})
+        
 
     top_artist = jsonify(artist_json)
 
@@ -74,18 +79,63 @@ def top_artist_timeframe_short():
 @app.route('/top-artist-long') 
 def top_artist_timeframe_long():
 
-    top_artist = recommendation.get_top_artists('long_term', 5)
+    top_artist = recommendation.get_top_artists('long_term', 50)
 
     artist_json = []
-    index = 1
+
     for artist in top_artist:
 
-        artist_json.append({"id": index, "name": artist[1], "image": artist[2]})
-        index += 1
+        artist_json.append({"name": artist[1], "image": artist[2]}) # get_top_songs returns tuple with artist uri, artist name and artist image, since we don't need uri, takes only items at index 1 and 2 which are name and image
 
     top_artist = jsonify(artist_json)
 
     return top_artist
+
+@app.route('/top-songs-short')
+def top_song_timeframe_short():
+
+    top_songs = recommendation.get_top_songs("short_term", 50)
+
+    song_json = []
+
+    for song in top_songs:
+
+        song_json.append({"song_name": song[0], "image": song[1], "artist": song[2]})
+
+    top_songs = jsonify(song_json)
+
+    return top_songs
+
+@app.route('/top-songs-medium')
+def top_song_timeframe_medium():
+
+    top_songs = recommendation.get_top_songs("medium_term", 50)
+
+    song_json = []
+
+    for song in top_songs:
+
+        song_json.append({"song_name": song[0], "image": song[1], "artist": song[2]})
+
+    top_songs = jsonify(song_json)
+
+    return top_songs
+
+@app.route('/top-songs-long')
+def top_song_timeframe_long():
+
+    top_songs = recommendation.get_top_songs("long_term", 50)
+
+    song_json = []
+
+    for song in top_songs:
+
+        song_json.append({"song_name": song[0], "image": song[1], "artist": song[2]})
+
+    top_songs = jsonify(song_json)
+
+    return top_songs
+
 
 @app.route('/gettime', methods=['POST'])
 def get_time():
@@ -94,7 +144,3 @@ def get_time():
     time = data['timeframe']
     return time
     
-
-if __name__ == '__main__':
-
-    app.run(debug=True)
